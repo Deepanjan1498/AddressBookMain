e6807f7ac6b3cb0f015bf8a6e045b027bd69a82d
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-01 04:32:45 +0530

    [Deepanjan]Add .uc6 is solved.

--- AddressBook.java
+++ AddressBook.java
@@ -10,6 +10,31 @@ public class AddressBook
 		contactArray=new ArrayList<>();
 		contactMap=new HashMap<>();
 	}
+	public void editContact()
+	{
+		System.out.println("Enter The First Name to edit the contact details");
+		String fn=sc.nextLine();
+		Contacts obj=contactMap.get(fn);
+		System.out.println();
+		System.out.println("Enter the Phone Number");
+		int num=sc.nextInt();
+		obj.setPhoneNumber(num);
+		System.out.println("Enter the EmailId");
+		String emId=sc.next();
+		obj.setEmailId(emId);
+		System.out.println("Enter the Address");
+		String add=sc.next();
+		obj.setAddress(add);
+		System.out.println("Enter the Pincode");
+		long zip=sc.nextLong();
+		obj.setZipcode(zip);
+		System.out.println("Enter the City");
+		String cit=sc.next();
+		obj.setCity(cit);
+		System.out.println("Enter the State");
+		String sta=sc.next();
+		obj.setState(sta);
+	 }
 	public void addNewContact()
 	{
 		System.out.println("Enter the First Name");
@@ -33,7 +58,7 @@ public class AddressBook
 		Contacts obj=new Contacts(firstname,lastname,phoneNumber,emailId,address,zipcode,city,state);
 		contactArray.add(obj);
 		contactMap.put(firstname,obj);
-	}
+	 }
 	public void printContact()
 	{
 	System.out.println(contactArray);
@@ -44,23 +69,27 @@ public class AddressBook
 		String fn=sc.nextLine();
 		Contacts obj=contactMap.get(fn);
 		contactArray.remove(obj);
-	}
+	 }
 	public static void main(String args[])
 	{
 		Scanner sd =new Scanner(System.in);
 		AddressBook Contactno1=new AddressBook();
 		while(true)
 		{
-		System.out.println("Do You Want to Add New Contact(Y/N)");
-		char choice=sd.next().charAt(0);
-			if(choice=='Y')
-			{
-			Contactno1.addNewContact();
-			Contactno1.printContact();
-			}
-			else
-			System.exit(0);
-		}
+		System.out.println("Enter  your choice \n1.Add new Contact \n2.Edit Contact \n3. Delete Contact \n4.Print Book \n5.Close");
+		int choice=sd.nextInt();
+		switch(choice){
+		case 1:Contactno1.addNewContact();
+		       break;
+		case 2:Contactno1.editContact();
+               break;		
+		case 3:Contactno1.deleteContact();
+               break;
+		case 4:Contactno1.printContact();       
+               break;
+		case 5:System.exit(0);;       
+		  }
+	    }	
 	}
 }
 
